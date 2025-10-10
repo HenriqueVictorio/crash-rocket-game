061887a3c5ffd54a3fe854ada373a865d674d1a3
HenriqueVictorio <166457263+HenriqueVictorio@users.noreply.github.com>
2025-10-10 08:45:36 -0300

    Update game.js

--- client/js/game.js
+++ client/js/game.js
@@ -16,7 +16,7 @@ class Game {
     this.startDelayTimeout = null;
     this.visibilityStartHandler = null;
     this.lastRenderTimestamp = 0;
-    this.targetFrameInterval = 10;
+    this.targetFrameInterval = 50;
         
         // Performance tracking
         this.lastFrameTime = 0;
